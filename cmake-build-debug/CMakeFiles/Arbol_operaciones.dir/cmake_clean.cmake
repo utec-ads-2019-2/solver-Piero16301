5f02d7661f5d226bf559a5a4dba9ce196bb7d02e
Piero Angelo <piero.morales@utec.edu.pe>
2019-09-25 20:10:05 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Arbol_operaciones.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Arbol_operaciones.dir/main.cpp.obj"
+  "Arbol_operaciones.pdb"
+  "Arbol_operaciones.exe"
+  "Arbol_operaciones.exe.manifest"
+  "libArbol_operaciones.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Arbol_operaciones.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
